58ae7cd6766a927cc2596aeb59ab5362c690a5b1
ricardo <ricardo@gmail.com>
2022-05-09 18:59:47 -0500

    Modify profile and add settings

--- app/src/main/java/example/com/healthpetapp/MenuActivity.java
+++ app/src/main/java/example/com/healthpetapp/MenuActivity.java
@@ -1,6 +1,8 @@
 package example.com.healthpetapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -52,6 +54,30 @@ public class MenuActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent settings = new Intent(this, Settings.class);
+            settings.putExtra("key", "settings");
+            this.startActivity(settings);
+        }
+
+        if (id == R.id.action_account) {
+            Intent profile = new Intent(this, Profile.class);
+            profile.putExtra("key", "profile");
+            this.startActivity(profile);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
