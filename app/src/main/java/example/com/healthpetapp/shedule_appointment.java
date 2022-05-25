30f203fb52c8a73185ccf4d6d258c45984211711
abrah <abrahamdaft@gmail.com>
2022-05-24 21:05:15 -0500

    fecha y hora

--- app/src/main/java/example/com/healthpetapp/shedule_appointment.java
+++ app/src/main/java/example/com/healthpetapp/shedule_appointment.java
@@ -2,13 +2,69 @@ package example.com.healthpetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
-public class shedule_appointment extends AppCompatActivity {
+import java.util.Calendar;
+
+public class shedule_appointment extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnCalendar, btnTime;
+    EditText calendar_text, time_text;
+    private  int dia, mes, año, hora, minutos;
+    Button btnSaveDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shedule_appointment);
+
+        btnCalendar = (Button) findViewById(R.id.cal);
+        btnTime = (Button) findViewById(R.id.tim);
+        calendar_text = (EditText) findViewById(R.id.calendar_text);
+        time_text = (EditText) findViewById(R.id.time_text);
+
+        btnCalendar.setOnClickListener(this);
+        btnTime.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btnCalendar){
+            final Calendar c = Calendar.getInstance();
+            dia= c.get(Calendar.DAY_OF_MONTH);
+            mes = c.get(Calendar.MONTH);
+            año = c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    calendar_text.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+
+                }
+            }
+            ,dia,mes,año);
+            datePickerDialog.show();
+
+        }if (v==btnTime){
+            final Calendar c = Calendar.getInstance();
+            hora= c.get(Calendar.HOUR_OF_DAY);
+            minutos= c.get(Calendar.MINUTE);
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    time_text.setText(hourOfDay+":"+minute);
+                }
+            }, hora, minutos, false);
+            timePickerDialog.show();
+
+        }
     }
 }
